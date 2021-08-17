c3739f30c5840c22a0c6a3a743b3ae8d36fb075a
Soumyo Pandey <70663140+Soumyo-Pandey@users.noreply.github.com>
2021-08-18 00:38:40 +0530

    Code for the project

---
+++ Speak the meaning of the word.py
@@ -0,0 +1,35 @@
+import pyttsx3
+from PyDictionary import PyDictionary
+
+
+class Speaking:
+
+    def speak(self, audio):
+        engine = pyttsx3.init('sapi5')
+
+
+        voices = engine.getProperty('voices')
+
+        engine.setProperty('voice', voices[0].id)
+        engine.say(audio)
+        engine.runAndWait()
+
+
+class GFG:
+    def Dictionary(self):
+        speak = Speaking()
+        dic = PyDictionary()
+        speak.speak("Which word do u want to find the meaning sir")
+
+        query = str(input())
+        word = dic.meaning(query)
+        print(len(word))
+
+        for state in word:
+            print(word[state])
+            speak.speak("the meaning  is" + str(word[state]))
+
+
+if __name__ == '__main__':
+    GFG()
+    GFG.Dictionary(self=None)
